d47f6d91c2ac26b1719709e7fd1ae347396aa54c
Zakhar Negreev-Dashkov <negreevdashkov.zakhar@gmail.com>
2022-08-01 13:25:35 +0600

    [ ]Refactored hepler file

--- tasks/helpers.ts
+++ tasks/helpers.ts
@@ -1,8 +1,14 @@
-const { ethers } = require("ethers");
-const { getContractAt } = require("@nomiclabs/hardhat-ethers/internal/helpers");
+import { Contract, ethers, Wallet } from "ethers";
+import { getContractAt } from "@nomiclabs/hardhat-ethers/internal/helpers";
+import {
+  BaseProvider,
+  InfuraProvider,
+  Provider,
+} from "@ethersproject/providers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 // Helper method for fetching environment variables from .env
-function getEnvVariable(key, defaultValue) {
+function getEnvVariable(key: string, defaultValue: any): any {
   if (process.env[key]) {
     return process.env[key];
   }
@@ -13,31 +19,35 @@ function getEnvVariable(key, defaultValue) {
 }
 
 // Helper method for fetching a connection provider to the Ethereum network
-function getProvider() {
-  return ethers.getDefaultProvider(getEnvVariable("NETWORK", "rinkeby"), {
+function getProvider(): Provider {
+  return new ethers.providers.InfuraProvider("rinkby", {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${getEnvVariable(
         "INFURA_PROJECT_ID",
+        "",
       )}`,
       chainId: 4,
     },
-  });
+  }) as unknown as Provider;
 }
 
 // Helper method for fetching a wallet account using an environment variable for the PK
-function getAccount() {
-  let wallet = ethers.Wallet.fromMnemonic(getEnvVariable("MNEMONIC"));
+function getAccount(): Wallet {
+  let wallet = ethers.Wallet.fromMnemonic(getEnvVariable("MNEMONIC", ""));
   wallet = wallet.connect(getProvider());
   return wallet;
 }
 
 // Helper method for fetching a contract instance at a given address
-export function getContract(contractName, hre) {
+export function getContract(
+  contractName: string,
+  hre: HardhatRuntimeEnvironment,
+): Promise<Contract> {
   const account = getAccount();
   return getContractAt(
     hre,
     contractName,
-    getEnvVariable("NFT_CONTRACT_ADDRESS"),
+    getEnvVariable("NFT_CONTRACT_ADDRESS", ""),
     account,
   );
 }
