fd3ad608250a8c418008630b18fa330e05be2893
Zakhar Negreev-Dashkov <negreevdashkov.zakhar@gmail.com>
2022-08-03 14:10:44 +0600

    [ ]Moved api keys to .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,9 +12,9 @@ import "./tasks";
 
 dotenv.config();
 const {
-  COIN_MARKET_CAP_API_KEY = "d25b5576-a4ee-41be-bb2b-aca2ba3ae5d8",
+  COIN_MARKET_CAP_API_KEY,
   ETHERSCAN_API_KEY,
-  INFURA_PROJECT_ID = "84842078b09946638c03157f83405213",
+  INFURA_PROJECT_ID,
   REPORT_GAS = "false",
   SOLIDITY_VERSION = "0.8.15",
 } = process.env;
